793d14ba88833068c2b8ffafa63e6e3536ce21e3
hjy <the398355565@gmail.com>
2020-11-05 11:30:32 +0800

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const path = require('path');
+
+function isExist(path) {
+    try{
+        const error = fs.accessSync(path);
+        return !error;
+    }catch(e){
+        return false;
+    }
+};
+
+const folder_path = "C:\\Users\\jhuang342\\Downloads\\123"
+let fnArr = fs.readdirSync(folder_path);
+
+const groupByUser = {};
+
+fnArr.forEach(fileName => {
+
+    //change mtime
+    let p = path.join(folder_path, fileName);
+    const mstr = fileName.match(/\d{4}-\d{2}-\d{2}/)[0];
+    if(mstr){
+        let mtime = new Date(mstr);
+        fs.utimesSync(p, mtime, mtime);
+    }
+
+    //prepare for grouping
+    const user =  fileName.split(" -- ")[0];
+    if(user.includes("@")){
+        groupByUser[user] = groupByUser[user] || [];
+        groupByUser[user].push(fileName);
+    }
+});
+
+let MKDIR_COUNT = 3;
+const output_path = "C:\\Users\\jhuang342\\Downloads";
+
+for(let user in groupByUser){
+    if(groupByUser.hasOwnProperty(user)){
+        const imgs = groupByUser[user];
+
+        if(imgs.length > MKDIR_COUNT){
+
+            try{
+                const dest = path.join(output_path, user);
+                if(!isExist(dest)){
+                    fs.mkdirSync(dest, {recursive: true});
+                }
+
+                imgs.forEach(e => {
+                    let newP = path.join(dest, e);
+                    let oldP = path.join(folder_path, e);
+                    fs.renameSync(oldP, newP)
+                })
+
+            }catch(e){
+                debugger;
+                console.error(e);
+            }
+        }
+    }
+}
+
+
+// if(!isExist(dest)){
+//      err = fs.mkdirSync(dest, {recursive: true});
+// }
+
+// fs.renameSync(oldPath, newPath)
