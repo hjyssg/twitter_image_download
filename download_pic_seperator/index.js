0a9acef19bef124f019842d014ade2c7a57b48f2
aji47 <the398355565@gmail.com>
2023-04-29 19:55:11 +0800

    doanload_pic_sep

--- download_pic_seperator/index.js
+++ download_pic_seperator/index.js
@@ -51,6 +51,7 @@ const isImage =  function (fn) {
 let rootPath = path.resolve(process.cwd()); 
 // let fp = process.argv[2]; //"Y:\\_Photo2\\test-1"
 let output_dir_path = path.resolve(rootPath, "sep_output");
+let author2fp = {};
 const main = async () => {
 
     const path_config_path = path.join(rootPath, "config-path.ini");
@@ -61,6 +62,8 @@ const main = async () => {
     for(let input_path of path_config["input_path"]){
         await handle_one_folder(input_path);
     }
+
+    await do_the_copying();
 }
 
 async function handle_one_folder(fp){
@@ -83,23 +86,52 @@ async function handle_one_folder(fp){
             if(!author){
                 continue;
             }
-            
-            const author_dir_path = path.resolve(output_dir_path, author);
-            await mkdir(author_dir_path);
-
             const tempFp = path.resolve(fp, fn);
-            const dest_fp = path.resolve(author_dir_path, fn);
-            if(await isExist(dest_fp)){
-                await pfs.copyFile(tempFp, dest_fp);
-            }
+            author2fp[author] = author2fp[author] || [];
+            author2fp[author].push(tempFp);
         }catch(e){
             console.error("["+ tempFp + "]", e)
         }finally{
-            console.log(`${ii}/${fileNames.length} FROM ${fp}`);
+            // console.log(`${ii}/${fileNames.length} FROM ${fp}`);
         }
     }
 }
 
+const MIN_PICS = 3;
+async function do_the_copying(){
+    const author_keys = Object.keys(author2fp);
+    for (let ii = 0; ii < author_keys.length; ii++){
+        const author = author_keys[ii];
+        let fps = author2fp[author]
+        if(!fps){
+            continue;
+        }
+
+        if(fps.length < MIN_PICS){
+            //图片太少的，就放外面
+            for(let tempFp of fps){
+                let fn = path.basename(tempFp);
+                const dest_fp = path.resolve(output_dir_path, fn);
+                if(!(await isExist(dest_fp))){
+                    await pfs.copyFile(tempFp, dest_fp);
+                }
+            }
+        }else{
+            //放进作者文件夹
+            const author_dir_path = path.resolve(output_dir_path, author);
+            await mkdir(author_dir_path);
+            for(let tempFp of fps){
+                let fn = path.basename(tempFp);
+                const dest_fp = path.resolve(author_dir_path, fn);
+                if(!(await isExist(dest_fp))){
+                    await pfs.copyFile(tempFp, dest_fp);
+                }
+            }
+        }
+
+         console.log(`${ii}/${author_keys.length} ${author}`);
+    }
+}
 
 main();
 
